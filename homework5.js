c83bf7a1a14f3d3b355d6300bc9641cc601dd208
innaghazaryan <156332121+innaghazaryan@users.noreply.github.com>
2024-02-21 00:07:03 +0400

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -34,3 +34,16 @@ function flattenArray(arr) {
   }
   return result;
 }
+// 6. Given a number. Write a function that calculates its sum of the digits and if that sum has more than 1 digit find the sum of digits of that number. Repeat that process if needed and return the result.
+function sumOfDigits(number) {
+  let sum = 0;
+  while (number > 0) {
+    sum += number % 10;
+    number = Math.floor(number / 10);
+  }
+  if (sum >= 10) {
+    return sumOfDigits(sum);
+  }
+
+  return sum;
+}
